d97e3b0a7f8d2a04374a2dc1066df2df2b5dce9d
常成 <PJLAB\changcheng@pjnl231040177l.pjlab.org>
2024-07-05 12:31:31 +0800

    get data from client graphql

--- app/dashboard/ckpts/client_layout.ts
+++ app/dashboard/ckpts/client_layout.ts
@@ -3,9 +3,49 @@ import { RGJsonData } from "relation-graph-react";
 
 
 export function layout(params: RoadmapQuery): RGJsonData {
+    const nodes: any[] = []
+    const lines: any[] = []
+    if (params.roadmap) {
+        params.roadmap.ckptList.forEach(node => {
+            const {id, config, ...others } = node
+            nodes.push({
+                id,
+                text: config,
+                myicon: "el-icon-star-on",
+                data: { ...others }
+            });
+        });
+        params.roadmap.trainConfigList.forEach(node => {
+            const {id, modelName, ...others } = node
+            nodes.push({
+                id,
+                text: modelName,
+                myicon: "el-icon-sunny",
+                data: { ...others }
+            });
+        });
+        params.roadmap.ckptStepList.forEach(line => {
+            const {id, from, to, ...others } = line
+            lines.push({
+                id, 
+                from, 
+                to,
+                data: { ...others }
+            });
+        });
+        params.roadmap.ckptStepList.forEach(line => {
+            const {id, from, to, ...others } = line
+            lines.push({
+                id, 
+                from, 
+                to,
+                data: { ...others }
+            });
+        });
+    }
     return {
-        rootId: "",
-        nodes: [],
-        lines: []
+        // rootId: "",
+        nodes,
+        lines
     };
 }
